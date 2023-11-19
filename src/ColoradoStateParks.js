3dc0b0bb300dd47e0b36c3e9ec67bf8160060c1f
mmwandera <mmwandera74@gmail.com>
2023-11-19 20:23:08 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,10 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import elevation from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  elevation(); 
 
   return <h1>Colorado State Parks!</h1>;
 }
